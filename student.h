0c3f38f7bc594fb370d1985377522ff7e60d7251
Anthony G Micodin <amicodin@o244-20.csuchico.edu>
2018-12-03 16:12:26 -0800

    added function protoypes

---
+++ student.h
@@ -0,0 +1,22 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_ 
+
+class student
+{
+        private:
+        public:
+  setName( string &first, string &last);
+  fullName();
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif
